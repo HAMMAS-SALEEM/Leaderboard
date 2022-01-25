5f6ae79bbf624ec42dec03be11fe05e363691885
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-25 15:50:40 +0500

    Fixed linters

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import './index.css'
+import './index.css';
 
-const playerBox = document.querySelector('.ranks')
+const playerBox = document.querySelector('.ranks');
 
-let player = [{
-        name: 'Hammas',
-        score: '100'
-    },
-    {
-        name: 'Jihane',
-        score: '500'
-    },
-    {
-        name: 'Tresor',
-        score: '300'
-    }
-]
+const player = [{
+  name: 'Hammas',
+  score: '100',
+},
+{
+  name: 'Jihane',
+  score: '500',
+},
+{
+  name: 'Tresor',
+  score: '300',
+},
+];
 
-player.forEach(play=>{
-   playerBox.innerHTML+=`<p class="player">${play.name} : ${play.score}</p>` 
-})
+player.forEach((play) => {
+  playerBox.innerHTML += `<p class="player">${play.name} : ${play.score}</p>`;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        }, ],
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    },
-    ),
-],
-mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+  }),
+  ],
+  mode: 'development',
 };
