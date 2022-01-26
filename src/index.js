019a71b87180d125ea8c3c0f13c97a0f331a35d6
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-26 17:55:59 +0500

    Update api data sending

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import './index.css';
 import refreshApiData from './components/js/api.js';
 
 const playerBox = document.querySelector('.ranks');
-const submitBtn = document.getElementById('refresh')
+const refreshBtn = document.getElementById('refresh')
+const form = document.getElementById('form')
+const name = document.getElementById('name')
+const score = document.getElementById('score')
 let players;
 
 window.addEventListener('load', () => {
   refreshApiData(players, playerBox);
 });
 
-submitBtn.addEventListener('click',()=>{
+refreshBtn.addEventListener('click',()=>{
   refreshApiData(players, playerBox);
+})
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  let obj = {
+    name: name.value,
+    score: score.value,
+    id: new Date().getTime().toString()
+  }
+  console.log(JSON.stringify(obj))
 })
