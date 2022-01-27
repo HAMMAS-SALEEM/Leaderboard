8948683320909710477b5787793a1d8ffcb1e3cc
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-27 18:53:03 +0500

    Update script for showRanks.js

--- src/components/js/showRanks.js
+++ src/components/js/showRanks.js
@@ -1,6 +1,8 @@
 const showRanks = (player, output) => {
+  let apiData = player.sort((a,b)=>parseFloat(b.score)-parseFloat(a.score))
   output.innerHTML = '';
-  player.forEach((play) => {
+  apiData.forEach((play,index) => {
+    if(index > 2 && index<10)
     output.innerHTML += `<p class="player">${play.user} : ${play.score}</p>`;
   });
 };
