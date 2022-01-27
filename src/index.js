decfc2d52bfe9d43d3ee4bc801fd28ee85f8c701
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-27 19:31:09 +0500

    Final Touches Done

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ const refreshBtn = document.getElementById('refresh');
 const form = document.getElementById('form');
 const name = document.getElementById('name');
 const score = document.getElementById('score');
-const firstName = document.querySelector('.first-person')
-const firstScore = document.querySelector('.first-score')
-const secondName = document.querySelector('.second-person')
-const secondScore = document.querySelector('.second-score')
-const thirdName = document.querySelector('.third-person')
-const thirdScore = document.querySelector('.third-score')
 let players;
 
 window.addEventListener('load', () => {
