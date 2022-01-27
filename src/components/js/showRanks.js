ab780d92d55a583ff51c8aef954d0d588cc70a16
Hammas-Saleem <hammassaleem376@gmail.com>
2022-01-27 19:15:53 +0500

    Finished JavaScript

--- src/components/js/showRanks.js
+++ src/components/js/showRanks.js
@@ -2,9 +2,20 @@ const showRanks = (player, output) => {
   let apiData = player.sort((a,b)=>parseFloat(b.score)-parseFloat(a.score))
   output.innerHTML = '';
   apiData.forEach((play,index) => {
-    if(index > 2 && index<10)
+    if(index==0) {
+      document.querySelector('.first-person').innerHTML = `${play.user}`
+      document.querySelector('.first-score').innerHTML = `${play.score}`
+    } else if(index==1) {
+      document.querySelector('.second-person').innerHTML = `${play.user}`
+      document.querySelector('.second-score').innerHTML = `${play.score}`
+    } else if(index==2) {
+      document.querySelector('.third-person').innerHTML = `${play.user}`
+      document.querySelector('.third-score').innerHTML = `${play.score}`
+    }
+    else if(index > 2 && index<10)
     output.innerHTML += `<p class="player">${play.user} : ${play.score}</p>`;
   });
 };
+
 
 export default showRanks;
